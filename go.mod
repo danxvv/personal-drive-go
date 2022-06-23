cd325146727b3b8b072fe0f2465ad03c9aef0b96
Daniel Mojica <46035416+danxvv@users.noreply.github.com>
2022-06-23 05:28:33 +0000

    trying uploads to a space

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 )
 
 require (
+	github.com/aws/aws-sdk-go v1.44.40 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,131 +1,65 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"strings"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-type User struct {
-	Name string
-	Age  int
+type File struct {
+	Name     string
+	Size     int64
+	Content  []byte
+	FileType string
 }
 
-type Post struct {
-	Id    int
-	Title string
-	Body  string
-}
-
-type GenericRepository[T any, V any] interface {
-	Get(id V) (T, error)
-	Post(T) (T, error)
-}
-
-type UserService struct {
-	UserRepository GenericRepository[User, int]
-}
-
-func NewUserService(userRepository GenericRepository[User, int]) *UserService {
-	return &UserService{
-		UserRepository: userRepository,
+func NewFile(name string, size int64, content []byte, fileType string) *File {
+	return &File{
+		Name:     name,
+		Size:     size,
+		Content:  content,
+		FileType: fileType,
 	}
-}
-
-func (u *UserService) Get(id int) (User, error) {
-	user, err := u.UserRepository.Get(id)
-	if err != nil {
-		return User{}, err
-	}
-	return user, nil
-}
-
-func (u *UserService) Post(user User) (User, error) {
-	user.Name = strings.ToUpper(user.Name)
-	dbUser, err := u.UserRepository.Post(user)
-	if err != nil {
-		return User{}, err
-	}
-	return dbUser, nil
-}
-
-type SQLLiteUserRepository struct {
-	DB *sql.DB
-}
-
-func NewSQLLiteUserRepository(url string) *SQLLiteUserRepository {
-	db, err := sql.Open("sqlite3", url)
-	if err != nil {
-		panic(err)
-	}
-	return &SQLLiteUserRepository{db}
-}
-
-func (u *SQLLiteUserRepository) Get(id int) (User, error) {
-	query := "SELECT name, age FROM users WHERE id = ?"
-	row := u.DB.QueryRow(query, id)
-	var name string
-	var age int
-	err := row.Scan(&name, &age)
-	if err != nil {
-		return User{}, err
-	}
-	return User{name, age}, nil
-}
-
-func (u *SQLLiteUserRepository) Post(user User) (User, error) {
-	query := "INSERT INTO users (name, age) VALUES (?, ?)"
-	_, err := u.DB.Exec(query, user.Name, user.Age)
-	if err != nil {
-		return User{}, err
-	}
-	return user, nil
-}
-
-func CreateTable(db *sql.DB) {
-	query := "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)"
-	_, err := db.Exec(query)
-	if err != nil {
-		panic(err)
-	}
-}
-
-type InMemoryUserRepository struct {
-	Users map[int]User
-}
-
-func NewInMemoryUserRepository() *InMemoryUserRepository {
-	return &InMemoryUserRepository{
-		Users: make(map[int]User),
-	}
-}
-
-func (u *InMemoryUserRepository) Get(id int) (User, error) {
-	user, ok := u.Users[id]
-	if !ok {
-		return User{}, fmt.Errorf("User not found")
-	}
-	return user, nil
-}
-
-func (u *InMemoryUserRepository) Post(user User) (User, error) {
-	u.Users[user.Age] = user
-	return user, nil
 }
 
 func main() {
-	dbRepository := NewSQLLiteUserRepository("users.db")
-	UserService := NewUserService(dbRepository)
-	user, err := UserService.Post(User{"Daniel", 1})
+	fileName := "video.mov"
+	f, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	fmt.Println(user)
-	user, err = UserService.Get(2)
+	file := NewFile(fileName, int64(len(f)), f, "video/quicktime")
+	key := os.Getenv("S3_KEY")
+	space := os.Getenv("S3_SPACE")
+	fmt.Println(key)
+	fmt.Println(space)
+
+	s3config := &aws.Config{
+		Credentials: credentials.NewStaticCredentials(key, space, ""),
+		Endpoint:    aws.String("fra1.digitaloceanspaces.com"),
+		Region:      aws.String("fra1"),
+	}
+	sess, err := session.NewSession(s3config)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	fmt.Println(user)
+	svc := s3.New(sess)
+	params := &s3.PutObjectInput{
+		Bucket:        aws.String("persona-danxvv-drive"),
+		Key:           aws.String(file.Name),
+		Body:          strings.NewReader(string(file.Content)),
+		ContentType:   aws.String(file.FileType),
+		ContentLength: aws.Int64(file.Size),
+	}
+	resp, err := svc.PutObject(params)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(resp)
 }
